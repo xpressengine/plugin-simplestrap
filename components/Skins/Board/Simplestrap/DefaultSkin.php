4ad60d728ebcceff063c50d4d151fbead928261f
bnu <bnufactory@gmail.com>
2018-10-18 15:41:10 +0900

    스킨이 잘못 설정된 문제 고침

---
+++ components/Skins/Board/Simplestrap/DefaultSkin.php
@@ -0,0 +1,9 @@
+<?php
+namespace Xpressengine\Plugins\Simplestrap\Components\Skins\Board\Simplestrap;
+
+use Xpressengine\Skin\GenericSkin;
+
+class DefaultSkin extends GenericSkin
+{
+    protected static $path = 'simplestrap/components/Skins/Board/Simplestrap';
+}
--- components/Skins/BoardWidget/Default/DefaultSkin.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Xpressengine\Plugins\Simplestrap\Skins;
-
-use Xpressengine\Skin\GenericSkin;
-
-class DefaultSkin extends GenericSkin
-{
-    protected static $path = 'simplestrap/components/Skins/BoardWidget/Default';
-}
